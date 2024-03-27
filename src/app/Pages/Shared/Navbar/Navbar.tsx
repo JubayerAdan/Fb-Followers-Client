1ea1208e61445b39015416618b7bd23ca61a925e
JubayerAdan <jubayeradan25@gmail.com>
2024-03-27 14:12:06 +0600

    first commit2

--- src/app/Pages/Shared/Navbar/Navbar.tsx
+++ src/app/Pages/Shared/Navbar/Navbar.tsx
@@ -1,32 +1,37 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import { FaShoppingCart } from "react-icons/fa";
-import { FaGem } from "react-icons/fa6";
+import { FaShoppingCart, FaGem } from "react-icons/fa";
 
 const Navbar = () => {
+  // Check if localStorage is available before accessing its value
+  const gems =
+    typeof window !== "undefined" ? Number(localStorage.getItem("gems")) : 0;
+
   const horizontal = (
     <ul className="menu menu-horizontal px-1">
       <li className="text-lg">
-        <Link href={"shop"}>
-          Shop <FaShoppingCart className="text-secondary"></FaShoppingCart>
+        <Link href="/shop">
+          <a>
+            Shop <FaShoppingCart className="text-secondary" />
+          </a>
         </Link>
       </li>
     </ul>
   );
 
   const vertical = (
-    <ul
-      tabIndex={0}
-      className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-    >
+    <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
       <li className="text-lg">
-        <Link href={"shop"}>
-          Shop <FaShoppingCart className="text-secondary"></FaShoppingCart>
+        <Link href="/shop">
+          <a>
+            Shop <FaShoppingCart className="text-secondary" />
+          </a>
         </Link>
       </li>
     </ul>
   );
+
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -54,10 +59,8 @@ const Navbar = () => {
       <div className="navbar-center hidden lg:flex">{horizontal}</div>
       <div className="navbar-end">
         <button className="btn">
-          <span className="text-lg">
-            {Number(localStorage.getItem("gems"))}
-          </span>{" "}
-          <FaGem className="text-xl text-secondary"></FaGem>
+          <span className="text-lg">{gems}</span>{" "}
+          <FaGem className="text-xl text-secondary" />
         </button>
       </div>
     </div>
